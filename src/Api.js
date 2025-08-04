7405e982c4c0bbccdfb8589b72de67626e801627
Hardik <pathakhardik2004@gmail.com>
2025-08-04 17:01:46 +0530

    links fixed

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_KEY = 'your_weather_api_key_here';
+const API_KEY = 'your_weather_api_key_here'
 const forecastEndpoint = params => `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`
 const locationsEndpoint = params => `http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${params.cityName}`
 const apiCall = async (endpoint) => {
